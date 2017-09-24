a4c4d6722fd4549b4191e51927d40fa00481d3a0
Kevin (Kun) "Kassimo" Qian <kevinkassimo@gmail.com>
2017-09-23 17:47:57 -0700

    Initial commit for Plugin

---
+++ ShootDust/SImpleFadeDust/USimpleFadeDust.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UDEngine;
+
+using DG.Tweening;
+
+namespace UDEngine.Plugin.ShootDust {
+	public static class USimpleFadeDust {
+		public static USimpleFadeDustObject Create(Transform transParent, Sprite sprite, bool useParentTransSettings = true, Vector3 position = default(Vector3), Quaternion rotation = default(Quaternion)) {
+			return new USimpleFadeDustObject (transParent, sprite, useParentTransSettings, position, rotation);
+		}
+	}
+
+	public class USimpleFadeDustObject {
+		public USimpleFadeDustObject(Transform transParent, Sprite sprite, bool useParentTransSettings = true, Vector3 position = default(Vector3), Quaternion rotation = default(Quaternion)) {
+			trans = new GameObject ("FadeDust").transform;
+
+			if (useParentTransSettings) {
+				trans.position = transParent.position;
+				trans.rotation = transParent.rotation;
+			} else {
+				trans.position = position;
+				trans.rotation = rotation;
+			}
+
+			trans.parent = transParent;
+
+			this.spriteRenderer = trans.gameObject.AddComponent<SpriteRenderer> ();
+			this.spriteRenderer.sprite = sprite;
+
+		}
+
+		public Transform trans;
+		public SpriteRenderer spriteRenderer;
+
+		public Tweener fadeTween;
+
+		public USimpleFadeDustObject Fade(float time) {
+			this.Reset ();
+
+			fadeTween = this.spriteRenderer.DOFade (0f, time);
+
+			return this;
+		}
+
+		public USimpleFadeDustObject Reset() {
+			if (fadeTween != null) {
+				fadeTween.Kill ();
+			}
+
+			Color lastColor = spriteRenderer.color;
+			lastColor.a = 1f;
+			spriteRenderer.color = lastColor;
+
+			return this;
+		}
+
+		public void Destroy() {
+			if (fadeTween != null) {
+				fadeTween.Kill ();
+				fadeTween = null;
+			}
+
+			MonoBehaviour.Destroy (trans.gameObject);
+
+			spriteRenderer = null;
+			trans = null;
+		}
+	}
+}
---
+++ THShoot/UTHShoot.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UDEngine.Core.Actor;
+using UDEngine.Core.Bullet;
+using UDEngine.Core.Collision;
+using UDEngine.Core.Pool;
+using UDEngine.Core.Shooter;
+
+using UDEngine.Commons.ShootStage;
+
+namespace UDEngine.Plugin.THShoot {
+	/// <summary>
+	/// Collection of shoot patterns that are similar or expanded from Touhou style
+	/// </summary>
+	public static class UTHShoot {
+		
+	}
+}
